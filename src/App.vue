512b9bf0262f5c5c0d396f0d614428b8bc62bdf1
yangshushan <yss_2016@outlook.com>
2019-06-07 10:00:04 +0800

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app>
+    <v-card class="mt-3 px-4">
+      <v-card-title class="title">{{type}}</v-card-title>
+      <v-radio-group v-model="type">
+        <v-radio v-for="{label, value1} of radioData" :key="value1" :label="label" :value="value1"/>
+      </v-radio-group>
+    </v-card>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      type: "selector",
+      radioData: [
+        { label: "Number", value1: "number" },
+        { label: "Selector", value1: "selector" },
+        { label: "Element", value1: "element" }
+      ]
+    };
+  }
+};
+</script>
